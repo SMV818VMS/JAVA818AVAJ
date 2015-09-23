ea8003261d6aedab6dab8c362f24920352624eae
Samuel Miravet Verde <samuelmiver@gmail.com>
2015-09-23 17:53:13 +0200

    Module 1.1. finished

---
+++ princeton/1_1_elements_of_programming/Initials.java
@@ -0,0 +1,27 @@
+/*----------------------------------------------------------------
+ *  Author:        Samuel Miravet-Verde
+ *  Written:       09/23/2015
+ *  Last updated:  09/23/2015
+ *
+ *  Compilation:   javac UseArgument.java
+ *  Execution:     java UseArgument
+ *  
+ *  Prints the initials K D W.
+ *
+ ******************************************************************************/
+
+public class Initials {
+
+    public static void main(String[] args) {
+        System.out.println("**        ***    **********      **             *             **");
+        System.out.println("**      ***      **        **     **           ***           ** ");
+        System.out.println("**    ***        **         **     **         ** **         **  ");
+        System.out.println("**  ***          **          **     **       **   **       **   ");
+        System.out.println("*****            **          **      **     **     **     **    ");
+        System.out.println("**  ***          **          **       **   **       **   **     ");
+        System.out.println("**    ***        **         **         ** **         ** **      ");
+        System.out.println("**      ***      **        **           ***           ***       ");
+        System.out.println("**        ***    **********              *             *        ");
+    }
+
+}
---
+++ princeton/1_1_elements_of_programming/crash_test_HelloWorld.java
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------
+ *  Author:        Samuel Miravet-Verde
+ *  Written:       09/23/2015
+ *  Last updated:  09/23/2015
+ *
+ *  Compilation:   javac HelloWorld.java
+ *  Execution:     java HelloWorld
+ *  
+ *  Prints "Hello, World". By tradition, this is everyone's
+ *  first program.
+ *
+ *  % java HelloWorld
+ *  Hello, World
+ *
+ *----------------------------------------------------------------*/
+
+public class HelloWorld {
+  public static void main(String[] ags) {
+    System.out.println("Hello, World");
+  }
+}
