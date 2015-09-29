8fcf9658cd5d4882242de682d88c29cdefbb34de
Samuel Miravet Verde <samuelmiver@gmail.com>
2015-09-29 11:34:18 +0200

    ex 27, Box Muller random number generator

--- princeton/1_2_built_in_types_of_data/creative_exercises/ex27_StdGaussian.java
+++ princeton/1_2_built_in_types_of_data/creative_exercises/ex27_StdGaussian.java
@@ -6,13 +6,6 @@
  *  Compilation:  javac ex27_StdGaussian.java
  *  Execution:    java ex27_StdGaussian
  *
- *  Read in Cartesian coordinates (x, y) and print out polar coordinates
- *  (r, theta).
- *
- *  %  java CartesianToPolar 3.0 4.0
- *  r     = 5.0
- *  theta = 0.9272952180016122
- *
  ******************************************************************************/
 
 public class ex27_StdGaussian {
@@ -21,7 +14,7 @@ public class ex27_StdGaussian {
         double u = Math.random();
         double v = Math.random();
 
-        double Z = (Math.sin(2*Math.PI*v))*Math.pow(-2*Math.log1p(u), 1/2);
+        double Z = (Math.sin(2 * Math.PI * v)) * Math.pow(-2 * Math.log1p(u), 1/2);
 
         System.out.println("u     = " + u);
         System.out.println("v     = " + v);
