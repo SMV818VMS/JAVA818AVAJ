2e98550628f9fd6f0134b76eeffa0ec91328058b
Samuel Miravet Verde <samuelmiver@gmail.com>
2015-10-02 11:32:43 +0200

    Alice game exercise

---
+++ princeton/1_2_built_in_types_of_data/web_exercises/ex10_AliceGame.java
@@ -0,0 +1,45 @@
+/*----------------------------------------------------------------
+ *  Author:        Samuel Miravet-Verde
+ *  Written:       10/02/2015
+ *  Last updated:  10/02/2015
+ *
+ *  Compilation:  javac ex10_AliceGame.java
+ *  Execution:    java ex10_AliceGame
+ *
+ *  Guess the biggest number. Consider the following game. Alice 
+ *  writes down two integers between 0 and 100 on two cards. Bob gets 
+ *  to select one of the two cards and see its value. After looking at 
+ *  the value, Bob commits to one of the two cards. If he chooses a card 
+ *  with the largest value, he wins; otherwise he loses. Devise a 
+ *  strategy (and corresponding computer program) for Bob so that 
+ *  he guarantees to win strictly more than half the time. 
+ *  
+ ******************************************************************************/
+
+public class ex10_AliceGame {
+    public static void main(String[] args) {
+
+        //Define the two random numbers of Alice
+        int A1 = 0 + (int) (Math.random() * 100);
+        int A2 = 0 + (int) (Math.random() * 100);
+
+        //Define the a random number for Bob
+        int B = 0 + (int) (Math.random() * 100);
+
+        //Bob accepts card A1 if A1>=B but swaps to A2 if A1<B.
+
+        int result = 0;
+        if(A1>=B) {
+              result += A1;
+        }else{
+              result += A2;
+        }
+
+        //Print
+        System.out.println("A1       = "+ A1);
+        System.out.println("A2       = "+ A2);
+        System.out.println("B        = "+ B);
+        System.out.println("Selected = "+ result);
+    }
+}
+
